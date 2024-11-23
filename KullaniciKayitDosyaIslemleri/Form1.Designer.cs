ebc23085ad435c37f54737f70e2501744589be20
Doğa Hava <101408033+dogahavaa@users.noreply.github.com>
2024-11-23 03:02:17 +0300

    Finito

--- KullaniciKayitDosyaIslemleri/Form1.Designer.cs
+++ KullaniciKayitDosyaIslemleri/Form1.Designer.cs
@@ -29,32 +29,31 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.btn_kayit = new System.Windows.Forms.Button();
-            this.tb_isim = new System.Windows.Forms.TextBox();
-            this.tb_soyisim = new System.Windows.Forms.TextBox();
-            this.tb_bolum = new System.Windows.Forms.TextBox();
-            this.tb_ogrencino = new System.Windows.Forms.TextBox();
             this.mtb_telefon = new System.Windows.Forms.MaskedTextBox();
+            this.tb_ogrencino = new System.Windows.Forms.TextBox();
+            this.tb_bolum = new System.Windows.Forms.TextBox();
+            this.tb_soyisim = new System.Windows.Forms.TextBox();
+            this.tb_isim = new System.Windows.Forms.TextBox();
+            this.btn_kayit = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.lb_kayitliOgrenciler = new System.Windows.Forms.ListBox();
             this.btn_bilgiGetir = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.gb_ogrenciBilgileri = new System.Windows.Forms.GroupBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
             this.lbl_telefon = new System.Windows.Forms.Label();
             this.lbl_bolum = new System.Windows.Forms.Label();
             this.lbl_ogrencino = new System.Windows.Forms.Label();
             this.lbl_isim = new System.Windows.Forms.Label();
             this.lbl_soyisim = new System.Windows.Forms.Label();
-            this.btn_sil = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.gb_ogrenciBilgileri.SuspendLayout();
@@ -80,50 +79,41 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Öğrenci Kayıt Formu";
             // 
-            // label1
+            // mtb_telefon
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "İsim :";
+            this.mtb_telefon.Location = new System.Drawing.Point(82, 143);
+            this.mtb_telefon.Mask = "(999) 000-0000";
+            this.mtb_telefon.Name = "mtb_telefon";
+            this.mtb_telefon.Size = new System.Drawing.Size(138, 20);
+            this.mtb_telefon.TabIndex = 10;
             // 
-            // label2
+            // tb_ogrencino
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 67);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Soyisim :";
+            this.tb_ogrencino.Location = new System.Drawing.Point(82, 117);
+            this.tb_ogrencino.Name = "tb_ogrencino";
+            this.tb_ogrencino.Size = new System.Drawing.Size(138, 20);
+            this.tb_ogrencino.TabIndex = 9;
             // 
-            // label3
+            // tb_bolum
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 120);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Öğrenci No :";
+            this.tb_bolum.Location = new System.Drawing.Point(82, 90);
+            this.tb_bolum.Name = "tb_bolum";
+            this.tb_bolum.Size = new System.Drawing.Size(138, 20);
+            this.tb_bolum.TabIndex = 8;
             // 
-            // label4
+            // tb_soyisim
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 93);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Bölüm :";
+            this.tb_soyisim.Location = new System.Drawing.Point(82, 64);
+            this.tb_soyisim.Name = "tb_soyisim";
+            this.tb_soyisim.Size = new System.Drawing.Size(138, 20);
+            this.tb_soyisim.TabIndex = 7;
             // 
-            // label5
+            // tb_isim
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(26, 146);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Telefon :";
+            this.tb_isim.Location = new System.Drawing.Point(82, 38);
+            this.tb_isim.Name = "tb_isim";
+            this.tb_isim.Size = new System.Drawing.Size(138, 20);
+            this.tb_isim.TabIndex = 6;
             // 
             // btn_kayit
             // 
@@ -136,63 +126,72 @@
             this.btn_kayit.UseVisualStyleBackColor = true;
             this.btn_kayit.Click += new System.EventHandler(this.btn_kayit_Click);
             // 
-            // tb_isim
+            // label5
             // 
-            this.tb_isim.Location = new System.Drawing.Point(82, 38);
-            this.tb_isim.Name = "tb_isim";
-            this.tb_isim.Size = new System.Drawing.Size(138, 20);
-            this.tb_isim.TabIndex = 6;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Telefon :";
             // 
-            // tb_soyisim
+            // label4
             // 
-            this.tb_soyisim.Location = new System.Drawing.Point(82, 64);
-            this.tb_soyisim.Name = "tb_soyisim";
-            this.tb_soyisim.Size = new System.Drawing.Size(138, 20);
-            this.tb_soyisim.TabIndex = 7;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bölüm :";
             // 
-            // tb_bolum
+            // label3
             // 
-            this.tb_bolum.Location = new System.Drawing.Point(82, 90);
-            this.tb_bolum.Name = "tb_bolum";
-            this.tb_bolum.Size = new System.Drawing.Size(138, 20);
-            this.tb_bolum.TabIndex = 8;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Öğrenci No :";
             // 
-            // tb_ogrencino
+            // label2
             // 
-            this.tb_ogrencino.Location = new System.Drawing.Point(82, 117);
-            this.tb_ogrencino.Name = "tb_ogrencino";
-            this.tb_ogrencino.Size = new System.Drawing.Size(138, 20);
-            this.tb_ogrencino.TabIndex = 9;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyisim :";
             // 
-            // mtb_telefon
+            // label1
             // 
-            this.mtb_telefon.Location = new System.Drawing.Point(82, 143);
-            this.mtb_telefon.Mask = "(999) 000-0000";
-            this.mtb_telefon.Name = "mtb_telefon";
-            this.mtb_telefon.Size = new System.Drawing.Size(138, 20);
-            this.mtb_telefon.TabIndex = 10;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim :";
             // 
             // lb_kayitliOgrenciler
             // 
             this.lb_kayitliOgrenciler.FormattingEnabled = true;
             this.lb_kayitliOgrenciler.Location = new System.Drawing.Point(26, 38);
             this.lb_kayitliOgrenciler.Name = "lb_kayitliOgrenciler";
-            this.lb_kayitliOgrenciler.Size = new System.Drawing.Size(169, 303);
+            this.lb_kayitliOgrenciler.Size = new System.Drawing.Size(169, 329);
             this.lb_kayitliOgrenciler.TabIndex = 1;
             // 
             // btn_bilgiGetir
             // 
             this.btn_bilgiGetir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn_bilgiGetir.Location = new System.Drawing.Point(26, 354);
+            this.btn_bilgiGetir.Location = new System.Drawing.Point(26, 386);
             this.btn_bilgiGetir.Name = "btn_bilgiGetir";
             this.btn_bilgiGetir.Size = new System.Drawing.Size(169, 28);
             this.btn_bilgiGetir.TabIndex = 2;
             this.btn_bilgiGetir.Text = "Bilgi Getir";
             this.btn_bilgiGetir.UseVisualStyleBackColor = true;
+            this.btn_bilgiGetir.Click += new System.EventHandler(this.btn_bilgiGetir_Click);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.btn_sil);
             this.groupBox2.Controls.Add(this.lb_kayitliOgrenciler);
             this.groupBox2.Controls.Add(this.btn_bilgiGetir);
             this.groupBox2.Location = new System.Drawing.Point(285, 12);
@@ -221,51 +220,6 @@
             this.gb_ogrenciBilgileri.TabStop = false;
             this.gb_ogrenciBilgileri.Text = "Öğrenci Bilgileri";
             this.gb_ogrenciBilgileri.Visible = false;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(44, 144);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(49, 13);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "Telefon :";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(51, 91);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(42, 13);
-            this.label8.TabIndex = 14;
-            this.label8.Text = "Bölüm :";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(26, 118);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(67, 13);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "Öğrenci No :";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(45, 65);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(48, 13);
-            this.label10.TabIndex = 12;
-            this.label10.Text = "Soyisim :";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(62, 39);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(31, 13);
-            this.label11.TabIndex = 11;
-            this.label11.Text = "İsim :";
             // 
             // lbl_telefon
             // 
@@ -312,16 +266,50 @@
             this.lbl_soyisim.TabIndex = 17;
             this.lbl_soyisim.Text = "Soyisim :";
             // 
-            // btn_sil
+            // label7
             // 
-            this.btn_sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn_sil.ForeColor = System.Drawing.Color.Brown;
-            this.btn_sil.Location = new System.Drawing.Point(26, 388);
-            this.btn_sil.Name = "btn_sil";
-            this.btn_sil.Size = new System.Drawing.Size(169, 28);
-            this.btn_sil.TabIndex = 3;
-            this.btn_sil.Text = "Sil";
-            this.btn_sil.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(44, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Telefon :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(51, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Bölüm :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(67, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Öğrenci No :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(62, 39);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "İsim :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(45, 65);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Soyisim :";
             // 
             // Form1
             // 
@@ -333,6 +321,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
@@ -370,7 +359,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Button btn_sil;
     }
 }
 
--- KullaniciKayitDosyaIslemleri/Form1.cs
+++ KullaniciKayitDosyaIslemleri/Form1.cs
@@ -13,15 +13,24 @@ namespace KullaniciKayitDosyaIslemleri
 {
     public partial class Form1 : Form
     {
+        string directoryPath = @"C:\Users\doga\Desktop\Ogrenciler";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb_kayitliOgrenciler.Items.Clear();
+            ListBoxaVeriCek();
         }
 
         private void btn_kayit_Click(object sender, EventArgs e)
         {
             if (DoluMu())
             {
+                //Öğrenci nesnesi oluştur.
                 Ogrenci o = new Ogrenci();
                 o.Isim = tb_isim.Text;
                 o.Soyisim = tb_soyisim.Text;
@@ -29,6 +38,7 @@ namespace KullaniciKayitDosyaIslemleri
                 o.Telefon = mtb_telefon.Text;
                 o.OgrenciNo = tb_ogrencino.Text;
 
+                //Klasör oluşturma işlemleri
                 string directoryPath = @"C:\Users\doga\Desktop\Ogrenciler";
                 DirectoryInfo di = new DirectoryInfo(directoryPath);
                 if (!di.Exists)
@@ -36,6 +46,7 @@ namespace KullaniciKayitDosyaIslemleri
                     di.Create();
                 }
 
+                //Dosya oluşturma ve yazma işlemleri 
                 string fileName = o.OgrenciNo;
                 string filePath = directoryPath + "/" + fileName + ".txt";
                 StreamWriter writer = new StreamWriter(filePath);
@@ -45,11 +56,41 @@ namespace KullaniciKayitDosyaIslemleri
                 writer.WriteLine(o.OgrenciNo);
                 writer.WriteLine(o.Telefon);
                 writer.Close();
+                lb_kayitliOgrenciler.Items.Clear();
+                ListBoxaVeriCek(); //Yeni veri eklendikten sonra listeyi sıfırla bir daha oluştur.
                 MessageBox.Show("Kayıt işlemi başarılı");
+
+                //Kayıt işleminden sonra textboxları temizle.
+                tb_isim.Text = "";
+                tb_soyisim.Text = "";
+                tb_bolum.Text = "";
+                tb_ogrencino.Text = "";
+                mtb_telefon.Text = "";
             }
             else
             {
                 MessageBox.Show("Bilgi alanları boş bırakılamaz.");
+            }
+        }
+
+        private void btn_bilgiGetir_Click(object sender, EventArgs e)
+        {
+            string[] ogrenciDosyalari = Directory.GetFiles(directoryPath); //Klasördeki dosyaları al.
+
+            foreach (string dosyaAdi in ogrenciDosyalari)
+            {
+                string[] satirlar = File.ReadAllLines(dosyaAdi);
+                string ogrAdSoyad = satirlar[0] + " " + satirlar[1];
+                string secili = lb_kayitliOgrenciler.SelectedItem.ToString();
+                if (secili == ogrAdSoyad)
+                {
+                    gb_ogrenciBilgileri.Visible = true;
+                    lbl_isim.Text = satirlar[0];
+                    lbl_soyisim.Text = satirlar[1];
+                    lbl_bolum.Text = satirlar[2];
+                    lbl_ogrencino.Text = satirlar[3];
+                    lbl_telefon.Text = satirlar[4];
+                }
             }
         }
 
@@ -78,6 +119,18 @@ namespace KullaniciKayitDosyaIslemleri
             else { return false; }
         }
 
+        private void ListBoxaVeriCek()
+        {
+            string[] ogrenciDosyalari = Directory.GetFiles(directoryPath); //Klasördeki dosyaları al.
 
+            foreach (string dosyaAdi in ogrenciDosyalari)
+            {
+                StreamReader sr = new StreamReader(dosyaAdi);
+                string ogrenciAdi = sr.ReadLine();
+                string ogrenciSoyadi = sr.ReadLine();
+                string ogrAdSoyad = ogrenciAdi + " " + ogrenciSoyadi;
+                lb_kayitliOgrenciler.Items.Add(ogrAdSoyad);
+            }
+        }
     }
 }
